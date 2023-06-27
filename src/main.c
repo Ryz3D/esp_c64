b1a69884fef1a8802c2d54d17bcc7d64ea456c51
Ryz3D <mirco.heitmann@gmail.com>
2023-06-27 22:11:45 +0200

    better f_cpu check

--- src/main.c
+++ src/main.c
@@ -1,51 +1,67 @@
 #include "main.h"
 
-int64_t last_irq = 0;
+int64_t t_f_cpu_start;
+int64_t t_last_irq = 0;
+
+void handle_uart_io()
+{
+    if (pc == 0xe5cd)
+    {
+        // siehe 0xEB35
+        uint8_t kb_index = (uint8_t)mem_read(0xc6);
+        if (kb_index < (uint8_t)mem_read(0x0289))
+        {
+            int c = getchar();
+            if (c > 0)
+            {
+                if (c == '\n')
+                    c = '\r';
+                mem_write(0x0277 + kb_index, c);
+                mem_write(0xc6, kb_index + 1);
+            }
+        }
+    }
+    if (pc == 0xe716)
+    {
+        if (a == '\r')
+            putchar('\n');
+        else if (a == 0x1d)
+            putchar(' ');
+        else if ((uint8_t)a != 0x93)
+            putchar(a);
+    }
+}
+
+void loop_f_cpu_check()
+{
+    if (pc == 0xc000)
+    {
+        t_f_cpu_start = esp_timer_get_time();
+    }
+    if (pc == 0xfce2)
+    {
+        int32_t t_duration = esp_timer_get_time() - t_f_cpu_start;
+        float f_cpu = 328714 / (t_duration / 100000.0);
+        printf("F_CPU = %i Hz\n", (int)f_cpu);
+    }
+}
 
 void c64_run(void *parameters)
 {
-    pc = 0xc000;
-    int64_t t_start = esp_timer_get_time();
     for (;;)
     {
         for (uint32_t i = 0; i < 10000; i++)
         {
-            if (esp_timer_get_time() - last_irq > 16666)
+            if (esp_timer_get_time() - t_last_irq > 16666)
             {
                 irq();
-                last_irq = esp_timer_get_time();
+                t_last_irq = esp_timer_get_time();
             }
             if (!gpio_get_level(42))
                 nmi();
 
-            if (pc == 0xfce2)
-            {
-                int64_t t_end = esp_timer_get_time();
-                int32_t t_duration = t_end - t_start;
-                printf("%i\n", t_duration);
-            }
-            if (pc == 0xe5cd)
-            {
-                int c = getchar();
-                if (c > 0)
-                {
-                    if (c == '\n')
-                        c = '\r';
-                    // siehe 0xEB35
-                    uint8_t kb_index = (uint8_t)mem_read(0xc6);
-                    mem_write(0x0277 + kb_index, c);
-                    mem_write(0xc6, kb_index + 1);
-                }
-            }
-            if (pc == 0xe716)
-            {
-                if (a == '\r')
-                    putchar('\n');
-                else if (a == 0x1d)
-                    putchar(' ');
-                else if ((uint8_t)a != 0x93)
-                    putchar(a);
-            }
+            handle_uart_io();
+            loop_f_cpu_check();
             exec_ins();
         }
         vTaskDelay(10 / portTICK_PERIOD_MS);
